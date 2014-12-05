a9683c40d53e888222f207ae9b2576279bf65848
MJGM <MJGM@example.com>
2014-12-05 13:55:24 +0100

    Métodos listFile y removeFile reescritos para evitar redundancia

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -10,15 +10,15 @@ public class MusicOrganizer
 {
     // An ArrayList for storing the file names of music files.
     private ArrayList<String> files;
-        
+
     /**
      * Create a MusicOrganizer
      */
     public MusicOrganizer()
     {
-            files = new ArrayList<>();
+        files = new ArrayList<>();
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
@@ -27,7 +27,7 @@ public class MusicOrganizer
     {
         files.add(filename);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -36,30 +36,34 @@ public class MusicOrganizer
     {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
      */
     public void listFile(int index)
     {
-        if(index >= 0 && index < files.size()) {
+        if(validIndex(index)) {
             String filename = files.get(index);
             System.out.println(filename);
         }
     }
-    
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
      */
     public void removeFile(int index)
     {
-        if(index >= 0 && index < files.size()) {
+        if(validIndex(index)) {
             files.remove(index);
         }
     }
-    
+
+    /**
+     * chequea que existe el indice introducido
+     */
+
     public void checkIndex(int index)
     {
         int max = this.files.size();
@@ -71,7 +75,19 @@ public class MusicOrganizer
         {
             System.out.println("Error rang 0-" + (max-1));
         }
-        
+
     }
-    
+
+    /**
+     * valida el indice 
+     */
+    private boolean validIndex(int index)
+    {
+        boolean valid = false;
+        if(index >= 0 && index < files.size())
+        {
+            valid = true;
+        }
+        return valid;
+    }          
 }
